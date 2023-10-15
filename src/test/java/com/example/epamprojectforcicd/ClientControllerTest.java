715138b955c683f76a12b85fa169424e7469d161
Davit Margaryan <dmargaryan88@gmail.com>
2023-10-15 21:39:09 +0400

    some necessary changes

--- src/test/java/com/example/epamprojectforcicd/ClientControllerTest.java
+++ src/test/java/com/example/epamprojectforcicd/ClientControllerTest.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ClientServiceImplTest {
+class ClientControllerTest {
 
     @InjectMocks
     private ClientServiceImpl clientService;
--- src/test/java/com/example/epamprojectforcicd/ToDoControllerTest.java
+++ src/test/java/com/example/epamprojectforcicd/ToDoControllerTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ToDoServiceImplTest {
+class ToDoControllerTest {
 
     @InjectMocks
     private ToDoServiceImpl toDoService;
