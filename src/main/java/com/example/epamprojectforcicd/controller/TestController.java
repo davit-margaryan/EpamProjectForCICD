3bed471c5e4a6b5fc5716cf842e22bc8f7273e0c
Davit Margaryan <dmargaryan88@gmail.com>
2023-10-16 00:25:15 +0400

    initial commit

---
+++ src/main/java/com/example/epamprojectforcicd/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.epamprojectforcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String testEndpoint() {
+        return "Hello, this is the test endpoint!";
+    }
+}
+
